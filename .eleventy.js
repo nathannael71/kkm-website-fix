4cd8651e89ea2dee1cb68fef30c32516e93ffd35
Nathan <88581278+nathannael71@users.noreply.github.com>
2025-04-19 02:00:36 +0700

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,12 @@
+const { DateTime } = require("luxon");
+
 module.exports = function(eleventyConfig) {
+  // Tambahkan filter 'date'
+  eleventyConfig.addFilter("date", (dateObj, format = "dd LLLL yyyy") => {
+    return DateTime.fromJSDate(new Date(dateObj)).toFormat(format);
+  });
+
+  // Salin folder 'images' dan 'admin'
   eleventyConfig.addPassthroughCopy("images");
   eleventyConfig.addPassthroughCopy("admin");
 
