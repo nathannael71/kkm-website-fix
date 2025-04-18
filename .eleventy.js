f93cb83c7cb7b2ef9e1b4c908f35b444cfcacbd4
Nathan <88581278+nathannael71@users.noreply.github.com>
2025-04-18 18:17:29 +0700

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,16 @@
-module.exports = function(eleventyConfig) {
-  eleventyConfig.addPassthroughCopy("admin"); // Penting! biar Netlify CMS jalan
+module.exports = function (eleventyConfig) {
+  // Supaya folder admin ikut dibuild
+  eleventyConfig.addPassthroughCopy("admin");
+
+  // Kumpulkan semua file di src/posts jadi koleksi "posts"
+  eleventyConfig.addCollection("posts", function (collectionApi) {
+    return collectionApi.getFilteredByGlob("src/posts/*.md");
+  });
+
   return {
     dir: {
-      input: ".",
+      input: "src",
       includes: "_includes",
-      data: "_data",
       output: "_site"
     }
   };
